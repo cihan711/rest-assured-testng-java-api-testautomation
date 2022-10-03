9078f89845b240ec1e1b370af620d193254cc4f0
Cihan <cihan@Cihan-MacBook-Pro.local>
2022-10-03 23:34:56 +0300

    Api test

---
+++ src/main/java/Software_Testing_Bootcamp/App.java
@@ -0,0 +1,13 @@
+package Software_Testing_Bootcamp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
